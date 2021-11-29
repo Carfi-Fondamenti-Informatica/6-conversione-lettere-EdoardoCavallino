649bdf9b220cb39e5893cc65655faf9e98d4678c
EdoardoCavallino <91631725+EdoardoCavallino@users.noreply.github.com>
2021-11-29 22:52:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
 
+#include "lib.h"
+
+int carattere(char a, char &ris){
+    bool car;
+
+    if((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z')){
+        car = true;
+
+        if(a >= 'A' && a <= 'Z'){
+            ris = a +32;
+        }
+        if(a >= 'a' && a <= 'z'){
+            ris = a -32;
+        }
+
+    }else{
+        car = false;
+    }
+    return car;
+}
