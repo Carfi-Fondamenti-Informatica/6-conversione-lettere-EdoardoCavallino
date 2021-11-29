a7d444532c040bfe78672302d09724ba7c851a38
EdoardoCavallino <91631725+EdoardoCavallino@users.noreply.github.com>
2021-11-29 22:54:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+
+    char x, lettera = 0;
+    cin >> x;
+
+    if ( carattere(x, lettera) == true ){
+        cout << lettera << endl;
+    }else{
+        cout << "errore" << endl;
+    }
+
+
+    return 0;
 }
+
+
+
